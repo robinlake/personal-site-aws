550a2d8cf1611ea887422fe1aaae69a17e4c4227
Robin Lake <mister.lake@gmail.com>
2017-08-15 10:40:25 -0600

    new resume photo

--- client/components/about/resumeInfo/modal.js
+++ client/components/about/resumeInfo/modal.js
@@ -4,9 +4,9 @@ var ModalWindow = React.createClass ({
   render(){
     return(
       <div>
-        <img src="/components/about/resumeInfo/resume.jpg" className="resumeSmall" data-target="#loginModal" data-toggle="modal"></img>
+        <img src="/components/about/resumeInfo/Resume-Large-min.png" className="resumeSmall" data-target="#loginModal" data-toggle="modal"></img>
         <div className="modal" id="loginModal" tabIndex="-1">
-          <img src="/components/about/resumeInfo/resume.jpg" data-target="#loginModal" data-toggle="modal"></img>
+          <img className="resume-modal" src="/components/about/resumeInfo/Resume-Large-min.png" data-target="#loginModal" data-toggle="modal"></img>
         </div>
       </div>
     );
