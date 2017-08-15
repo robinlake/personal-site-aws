c15a6b3c66fe841b278dd2d92c1db639d2ac32ee
Robin Lake <mister.lake@gmail.com>
2017-08-15 10:24:04 -0600

    clear social links

--- client/components/contact/socialLinks.js
+++ client/components/contact/socialLinks.js
@@ -4,8 +4,8 @@ export class SocialLinks extends React.Component{
   render(){
     return (
       <div id="linkList">
-        <a href="https://github.com/robinlake" target="_blank"><img className="mediaLink linkedin" src="/components/contact/linkedin.jpg"></img></a>
-        <a href="https://www.linkedin.com/in/robin-lake-9a9193137/" target="_blank"><img className="mediaLink github" src="/components/contact/github.png"></img></a>
+        <a href="https://github.com/robinlake" target="_blank"><img className="mediaLink linkedin" src="/components/contact/linkedin-clear.png"></img></a>
+        <a href="https://www.linkedin.com/in/robin-lake-9a9193137/" target="_blank"><img className="mediaLink github" src="/components/contact/github-clear.png"></img></a>
       </div>
 
     );
