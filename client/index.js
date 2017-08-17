b5293a6da9bf469335569b8566ef29ed19ebfebe
Robin Lake <mister.lake@gmail.com>
2017-08-17 08:19:06 -0600

    Remove extra navbar file

--- client/index.js
+++ client/index.js
@@ -6,20 +6,16 @@ import { Projects } from './components/projects/projects';
 import { About } from './components/about/about';
 import { Contact } from './components/contact/contact';
 import { MainNavbar } from './components/navbar/navbar';
-import { ReactNav } from './components/navbar/navbar2';
-// import { Particles } from './components/title/particles/particlesComponent';
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        {/*<MainNavbar />*/}
-        {/*<Particles />*/}
-          <ReactNav />
-          <Title />
-          <About />
-          <Projects />
-          <Contact />
+        <MainNavbar />
+        <Title />
+        <About />
+        <Projects />
+        <Contact />
       </div>
     );
   }
